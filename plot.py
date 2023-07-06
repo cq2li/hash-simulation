7646b7fef5e8cd1044d872aebf79453c623bf0e3
George <cqli1773@gmail.com>
2023-07-05 23:25:49 -0700

    Time Averaged Insertion and Load

--- plot.py
+++ plot.py
@@ -4,194 +4,232 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dir = Path(".")
-data = list(dir.glob("*rec.txt"))
-
-agg = []
-
-for dat in data:
-    df = pd.read_csv(dat.name, delim_whitespace=True, skiprows=2)
-    desc = dat.open().readline().strip()
-    df['description'] = desc
-    df['filename'] = dat.name
-    agg.append(df)
-
-agg = pd.concat(agg)
-
-plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        agg[agg.freeFraction < 0.3],
-        x = "freeFraction",
-        y = "unsuccessfull",
-        hue = "description",
-        )
-sim.set(title = "Reconcile with paper",
-        xlabel = "Free Fraction")
-plt.xlim(reversed(plt.xlim()))
-plt.savefig("Unsuc_freeFrac_rec")
-# plt.show()
-
-plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        agg[agg.freeFraction < 0.3],
-        x = "freeFraction",
-        y = "successfull",
-        hue = "description",
-        )
-sim.set(title = "Reconcile with paper",
-        xlabel = "Free Fraction")
-plt.xlim(reversed(plt.xlim()))
-plt.savefig("Suc_freeFrac_rec")
-# plt.show()
-
-plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        agg[agg.freeFraction < 0.3],
-        x = "K",
-        y = "unsuccessfull",
-        hue = "description")
-sim.set(title = "Reconcile with paper",
-        xlabel = "Number of deletions")
-plt.savefig("Unsuc_ndelete_rec")
-# plt.show()
-
-plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        agg[agg.freeFraction < 0.3],
-        x = "K",
-        y = "successfull",
-        hue = "description")
-sim.set(title = "Reconcile with paper",
-        xlabel = "Number of deletions")
-plt.savefig("Suc_ndelete_rec")
-# plt.show()
-
-plot_data = list(dir.glob("./plot/*plot.txt"))
+sns.set_style("whitegrid", {"grid.linestyle": ":"})
+plot_data = list(dir.glob("./plot_data/v2/*plot.txt"))
 plot_agg = []
 
 for dat in plot_data:
     df = pd.read_csv(dat, delim_whitespace=True, skiprows=2)
     desc = dat.open().readline().strip()
-    df['description'] = desc
     df['filename'] = dat.name
     plot_agg.append(df)
 
 plot_agg = pd.concat(plot_agg)
-plot_agg = plot_agg.sort_values(by="description")
 plot_agg["tombstones"] = 1 - plot_agg["freeFraction"].add(plot_agg["n"], axis=0)
-plot_agg["sqrt_unsuccessfull"] = plot_agg["unsuccessfull"]**(-1/2)
-# print(plot_agg)
+plot_agg["sqrt_unsuccessful-search"] = plot_agg["unsuccessful-search"]**(-1/2)
 
-plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        plot_agg,
-        x = "freeFraction",
-        y = "unsuccessfull",
-        hue = "description",
-        )
-sim.set(title = "Stochastic Unsuccessful Search Cost",
-        xlabel = "Free slots (not occupied nor tombstone)")
-plt.xlim(reversed(plt.xlim()))
-plt.savefig("Unsuc_freeFrac_plot")
+# plt.figure(figsize=(13.75, 10))
+# sim = sns.lineplot(
+#         plot_agg,
+#         x = "freeFraction",
+#         y = "unsuccessful-search",
+#         hue = "k",
+#         )
+# sim.set(title = "Stochastic Unsuccessful Search Cost",
+#         xlabel = "Free slots not occupied nor tombstone)")
+# plt.xlim(reversed(plt.xlim()))
+# plt.savefig("Unsuc_freeFrac_plot")
+# plt.show()
+#
+# plt.figure(figsize=(13.75, 10))
+# sim = sns.lineplot(
+#         plot_agg,
+#         x = "freeFraction",
+#         y = "successful-search",
+#         hue = "k",
+#         )
+# sim.set(title = "Stochastic Successful Search Cost",
+#         xlabel = "Free slots (not occupied nor tombstone)")
+# plt.xlim(reversed(plt.xlim()))
+# plt.savefig("Suc_freeFrac_plot")
 # plt.show()
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.lineplot(
-        plot_agg,
-        x = "freeFraction",
-        y = "successfull",
-        hue = "description",
-        )
-sim.set(title = "Stochastic Successful Search Cost",
-        xlabel = "Free slots (not occupied nor tombstone)")
-plt.xlim(reversed(plt.xlim()))
-plt.savefig("Suc_freeFrac_plot")
-# plt.show()
-
-plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        plot_agg.sort_values(by="description"),
+        plot_agg.sort_values(by="k"),
         x = "K",
-        y = "unsuccessfull",
-        hue = "description")
-sim.set(title = "Stochastic Unsuccessful Search Cost",
-        xlabel = "Number of deletions")
+        y = "unsuccessful-search",
+        hue = "k",
+        legend = "full")
+sim.set(title = "Unsuccessful Search Cost Over Time", xlabel = "Number of Deletions")
 plt.yscale('log')
-plt.savefig("Unsuc_ndelete_plot")
-# plt.show()
+plt.savefig("Unsuccessful Search Cost Over Time")
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.lineplot(
         plot_agg,
         x = "K",
-        y = "successfull",
-        hue = "description")
-sim.set(title = "Stochastic Successful Search Cost",
-        xlabel = "Number of deletions")
+        y = "successful-search",
+        hue = "k",
+        legend = "full")
+sim.set(title = "Successful Search Cost Over Time", xlabel = "Number of Deletions")
 plt.yscale('log')
-plt.savefig("Suc_ndelete_plot")
+plt.savefig("Successful Search Cost Over Time")
+
+
+plt.figure(figsize=(13.75, 10))
+sim = sns.lineplot(
+        plot_agg,
+        x = "K",
+        y = "timeavg-insertion",
+        hue = "k",
+        legend = "full")
+sim.set(title = "Time Averaged Insertion Cost", xlabel = "Number of Deletions")
+plt.savefig("Time Averaged Inertion Cost")
+
+
+rows = plot_agg.groupby("k")["K"].transform(max) == plot_agg["K"]
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.scatterplot(
-        plot_agg[plot_agg.K == 16777215],
+        plot_agg[rows],
         x = "k",
-        y = "unsuccessfull",
-        hue = "description")
-sim.set(title = "Stochastic Unsuccessful Search Cost On Last Iteration",
-        xlabel = "k - stochastic weight")
+        y = "unsuccessful-search",
+        hue = "k",
+        legend = "full")
+sim.set(title = "Unsuccessful Search Cost On Last Iteration Interval", xlabel = "k - Stochastic Weight")
 plt.yscale('log')
-plt.savefig("Unsuc_w_plot")
+plt.savefig("Unsuccssful Search Cost On Last Iteration Interval")
 
+plt.figure(figsize=(13.75, 10))
+sim = sns.scatterplot(
+        plot_agg[rows],
+        x = "k",
+        y = "successful-search",
+        hue = "k",
+        legend = "full")
+sim.set(title = "Successful Search Cost On Last Iteration Interval", xlabel = "k - Stochastic Weight")
+plt.savefig("Successful Search Cost On Last Iteration Interval")
+
+plt.figure(figsize=(13.75, 10))
+sim = sns.scatterplot(
+        plot_agg[rows],
+        x = "k",
+        y = "timeavg-insertion",
+        legend = "full")
+sim.set(title = "Time Average Insertion Cost On Last Iteration Interval", xlabel = "k - Stochastic Weight")
+plt.savefig("Time Average Insertion on Last Iteration Interval vs k")
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.scatterplot(
         plot_agg,
         x = "freeFraction",
-        y = "unsuccessfull",
-        hue = "description")
-sim.set(title = "Stochastic Unsucc. Search vs Fraction Free (excl. Tmb)",
-        xlabel = "FreeFraction")
+        y = "unsuccessful-search",
+        hue = "k",
+        legend = "full")
+sim.set(title = "Unsuccessful Search Cost vs Empty Slots", xlabel = "Empty Slots Excluding Tombstone")
 plt.yscale('log')
-plt.savefig("Unsuc_freeFrac_plot")
+plt.savefig("Unsuccessful Search Cost vs Empty Slots")
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.scatterplot(
         plot_agg,
         x = "freeFraction",
-        y = "sqrt_unsuccessfull",
-        hue = "description")
+        y = "sqrt_unsuccessful-search",
+        hue = "k")
 sim.set(title = "Stochastic Unsucc. Search vs Fraction Free (excl. Tmb)",
         xlabel = "FreeFraction")
-plt.savefig("Unsuc_freeFrac_nonlog_plot")
+plt.savefig("Sqrt")
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.lineplot(
         plot_agg,
         x = "K",
         y = "freeFraction",
-        hue = "k")
-sim.set(title = "Stochastic Fraction Free (excl Tmb) vs #deletions",
-        xlabel = "# of deletions")
+        hue = "k",
+        legend = "full")
+sim.set(title = "Empty Slots Over Time", xlabel = "Number of Deletions", ylabel = "Empty Slots")
 plt.yscale('log')
-plt.savefig("freeFrac_ndeletes_plot")
+plt.savefig("Empty Slots Over Time")
 
 plt.figure(figsize=(13.75, 10))
 sim = sns.lineplot(
         plot_agg,
         x = "K",
         y = "n",
-        hue = "k")
-sim.set(title = "Stochastic Load Over time",
-        xlabel = "# of deletions")
-plt.savefig("AvgLoad_ndeletes_plot")
+        hue = "k",
+        legend = "full")
+sim.set(title = "Load (n/m) Over time", xlabel = "Number of Deletions")
+plt.savefig("Load Over Time")
 
 # seaborne does this werid thing where it buckets your hue values
-hue_plot = plot_agg.sort_values(by="E[Load]")
 plt.figure(figsize=(13.75, 10))
-sim = sns.lineplot(
-        hue_plot,
-        x = "K",
+sim = sns.scatterplot(
+        plot_agg[rows],
+        x = "k",
         y = "tombstones",
-        hue = ["$%s$" % x for x in hue_plot["E[Load]"]])
-sim.set(title = "Stochastic tombstones Over Time",
-        xlabel = "# of deletions")
-plt.savefig("Avgtombstones_ndeletes_plot")
+        hue = "k",
+        legend = "full")
+sim.set(title = "Tombstones On Last Iteration Interval", xlabel = "k - Stochastic Weight")
+plt.savefig("Tombstone on Last Iteration Interval vs k")
+
+plt.figure(figsize=(13.75, 10))
+sim = sns.scatterplot(
+        plot_agg[rows],
+        x = "k",
+        y = "timeavg-load",
+        legend = "full")
+sim.set(title = "Load (n/m) at Last 10% of Iterationtion", xlabel = "k - Stochastic Weight")
+plt.savefig("Time Average Load Last 10pct Iterations")
+
+
+# data = list(dir.glob("*rec.txt"))
+#
+# agg = []
+#
+# for dat in data:
+#     df = pd.read_csv(dat.name, delim_whitespace=True, skiprows=2)
+#     desc = dat.open().readline().strip()
+#     df['k'] = desc
+#     df['filename'] = dat.name
+#     agg.append(df)
+#
+# agg = pd.concat(agg)
+#
+# plt.figure(figsize=(13.75, 10))
+# sim = sns.lineplot(
+#         agg[agg.freeFraction < 0.3],
+#         x = "freeFraction",
+#         y = "unsuccessful-search",
+#         hue = "k",
+#         )
+# sim.set(title = "Reconcile with paper",
+#         xlabel = "Free Fraction")
+# plt.xlim(reversed(plt.xlim()))
+# plt.savefig("Unsuc_freeFrac_rec")
+# # plt.show()
+#
+# plt.figure(figsize=(13.75, 10))
+# sim = sns.lineplot(
+#         agg[agg.freeFraction < 0.3],
+#         x = "freeFraction",
+#         y = "successful-search",
+#         hue = "k",
+#         )
+# sim.set(title = "Reconcile with paper",
+#         xlabel = "Free Fraction")
+# plt.xlim(reversed(plt.xlim()))
+# plt.savefig("Suc_freeFrac_rec")
+# # plt.show()
+#
+# plt.figure(figsize=(13.75, 10))
+# sim = sns.lineplot(
+#         agg[agg.freeFraction < 0.3],
+#         x = "K",
+#         y = "unsuccessful-search",
+#         hue = "k")
+# sim.set(title = "Reconcile with paper",
+#         xlabel = "Number of deletions")
+# plt.savefig("Unsuc_ndelete_rec")
+# # plt.show()
+#
+# plt.figure(figsize=(13.75, 10))
+# sim = sns.lineplot(
+#         agg[agg.freeFraction < 0.3],
+#         x = "K",
+#         y = "successful-search",
+#         hue = "k")
+# sim.set(title = "Reconcile with paper",
+#         xlabel = "Number of deletions")
+# plt.savefig("Suc_ndelete_rec")
+# # plt.show()
--- plot.sh
+++ plot.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 # echo Please give a description of the setup
 # read description
+# for k in 1 2 3 4 5
+#   do
+#     file=${k}_$(date +"%s_plot").txt
+#     echo k = $k > $file
+#     ./sim_plot.o 1000000 $k $((2 ** 22)) 1996 >> $file
+#   done
+#
+# for k in 9 10 11 19 20 21 24 49 99 ## runs forever, cut down on iterations
+#   do
+#     file=${k}_$(date +"%s_plot").txt
+#     echo k = $k > $file
+#     ./sim_plot.o 1000000 $k $((2 ** 21)) 1996 >> $file
+#   done
+
 for k in 1 2 3 4 5
   do
     file=${k}_$(date +"%s_plot").txt
     echo k = $k > $file
-    ./sim_plot.o 1000000 $k $((2 ** 23)) 1996 >> $file
-  done
-
-for k in 9 10 11 19 20 21 24 49 99 ## runs forever, cut down on iterations
-  do
-    file=${k}_$(date +"%s_plot").txt
-    echo k = $k > $file
-    ./sim_plot.o 1000000 $k $((2 ** 21)) 1996 >> $file
+    ./sim_plot.o 1000000 $k $((2 ** 22)) 1996 >> $file
   done
