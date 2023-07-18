c635bcd563494c04e2c10d1740b03759dea71ef2
George <cqli1773@gmail.com>
2023-07-18 02:49:43 -0700

    Remove misc. files

--- rec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo Please give a description of the setup
-read description
-file=$(date +"%Y%m%d_%H%M%S%p_rec").txt
-echo $description > $file
-./sim.o 1000000 4 100000000 1996 >> $file &
